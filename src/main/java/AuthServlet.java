b4b818ae74493c5672c7dcbca1333de3554f02ec
Thor77 <thor77@thor77.org>
2018-06-22 15:31:56 +0200

    Rewrite authentication to use User-model

--- src/main/java/AuthServlet.java
+++ src/main/java/AuthServlet.java
@@ -1,29 +1,37 @@
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        try {
 
-            DatabaseManager dbmgr = new DatabaseManager();
-            if (dbmgr.verifyUserCredentials(email, password)) {
-                System.out.println("Success");
-                req.getSession().setAttribute("email", email);
-                req.getServletContext().getRequestDispatcher("/authenticated.jsp").forward(req, resp);
-            } else {
-                System.out.println("Unauthorized");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        if (email == null || password == null) {
+            System.out.println("Email/Username or password missing");
+            return;
+        }
+        Session session = HibernateUtils.getSession();
+        Query query = session.getNamedQuery("findUserByEmail");
+        query.setParameter("email", email);
+        User user = (User) query.uniqueResult();
+        if (user == null) {
+            System.out.println("User doesn't exist");
+            return;
+        }
+        if (user.getPassword().equals(password)) {
+            System.out.println("Auth successful");
+            req.getSession().setAttribute("email", email);
+            req.getServletContext().getRequestDispatcher("/authenticated.jsp").forward(req, resp);
+        } else {
+            System.out.println("User found, auth not successful");
         }
     }
 }
