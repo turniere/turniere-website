64682eca016e275ed397baabef437834c965739c
Thor77 <thor77@thor77.org>
2018-06-24 19:39:16 +0200

    Mark username and email field as unique

--- src/main/java/database/models/User.java
+++ src/main/java/database/models/User.java
@@ -19,8 +19,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(unique = true)
     private String username;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
