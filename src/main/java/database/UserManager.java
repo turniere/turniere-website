a27c475e135f6e098895989d39727ae126e1f6a5
Malaber <32635600+Malaber@users.noreply.github.com>
2018-06-24 17:58:50 +0200

    add register method to userManager

--- src/main/java/database/UserManager.java
+++ src/main/java/database/UserManager.java
@@ -2,7 +2,9 @@ package database;
 
 import database.models.User;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 
 public class UserManager extends DatabaseManager {
     public static boolean verifyCredentials(String email, String password) {
@@ -15,5 +17,19 @@ public class UserManager extends DatabaseManager {
         if (user == null) return false;
         // compare passwords
         return user.getPassword().equals(password);
+    }
+
+    public void register(String username, String email, String password){
+
+        Session session = getSession();
+        Transaction transaction = session.getTransaction();
+        User user = new User(username, email, password);
+
+        session.save(user);
+        if (transaction.getStatus().equals(TransactionStatus.ACTIVE)) {
+            System.out.println("Committing transaction");
+            transaction.commit();
+        }
+
     }
 }
--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import database.UserManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,18 +11,33 @@ import java.io.IOException;
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String repassword = req.getParameter("repassword");
-        String prename = req.getParameter("prename");
-        String lastname = req.getParameter("lastname");
+
+        System.out.println("Request to add :");
+        System.out.println("Username: "+username);
+        System.out.println("E-Mail:     "+email);
+        System.out.println("Password:   "+password);
+        System.out.println("RePassword: "+repassword);
+
+
+        UserManager userManager = new UserManager();
+
+        if (password.equals(repassword)) {
+
+            userManager.register(username,email,password);
+
+            System.out.println("Added User to Database");
+
+        } else {
+            System.out.println("Passwords don't match");
+            //redirect to same page with password not matching warning
+        }
         /*try {
 
             DatabaseManager dbmgr = new DatabaseManager();
-            System.out.println("Request to add :");
-            System.out.println("E-Mail:     "+email);
-            System.out.println("Password:   "+password);
-            System.out.println("RePassword: "+repassword);
             if (password.equals(repassword)) {
                 if (!dbmgr.doesMailExist(email)){
                     dbmgr.addUser(prename,lastname,email,password);
