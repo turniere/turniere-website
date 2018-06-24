bd23780da2595660b49945c95eaa554a0ae87bb8
Thor77 <thor77@thor77.org>
2018-06-24 15:33:28 +0200

    Overwrite DatabaseManger with HibernateUtils
    Model managers can now extend DatabaseManager to get access to a Hibernate session.
    This prohibits direct access to a Hibernate session (at least through this class), which should always be abstracted by a model manager.

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,47 +1,26 @@
-import models.User;
+package database;
+
+import database.models.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import java.sql.*;
 
-public class DatabaseManager {
-    Connection databaseConnection;
+abstract class DatabaseManager {
+    private static final SessionFactory ourSessionFactory;
 
-    public DatabaseManager() throws SQLException, ClassNotFoundException {
-
-        String sqlitePath = "database.sqlite";
-
-        Class.forName("org.sqlite.JDBC");
-        databaseConnection = DriverManager.getConnection("jdbc:sqlite:" + sqlitePath);
-    }
-
-    public boolean verifyUserCredentials(String email, String password) {
-        Session session = HibernateUtils.getSession();
-        Query query = session.getNamedQuery("findUserByEmail");
-        query.setParameter("email", email);
-        User user = (User) query.uniqueResult();
-        if (user == null) {
-            System.out.println("User doesn't exist");
-            return false;
+    static {
+        try {
+            ourSessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
         }
-        return user.getPassword().equals(password);
     }
 
-    public boolean doesMailExist(String email) throws SQLException {
-        final String statement = "SELECT email FROM users where email=?";
-        PreparedStatement ps = databaseConnection.prepareStatement(statement);
-        ps.setString(1, email);
-        ResultSet result = ps.executeQuery();
-        return result.next();
-    }
-
-    public void addUser(String firstName, String lastName, String email, String password) throws SQLException {
-        final String statement = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
-        PreparedStatement ps = databaseConnection.prepareStatement(statement);
-        ps.setString(1, firstName);
-        ps.setString(2, lastName);
-        ps.setString(3, email);
-        ps.setString(4, password);
-        ps.executeUpdate();
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
     }
 }
