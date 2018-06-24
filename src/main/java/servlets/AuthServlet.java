1d2867883320b29d7c7ae6b38c29c3b37a20a42e
Malaber <32635600+Malaber@users.noreply.github.com>
2018-06-24 18:01:13 +0200

    readd password check in auth servlet

--- src/main/java/servlets/AuthServlet.java
+++ src/main/java/servlets/AuthServlet.java
@@ -11,15 +11,22 @@ import java.io.IOException;
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String email = req.getParameter("email");
         String password = req.getParameter("password");
+        String email = req.getParameter("email");
+
+        //check if parameters are present
+        if (password == null || email == null) {
+            System.out.println("Email or password missing");
+            return; //TODO return to login page with message to specify both
+        }
 
         if (UserManager.verifyCredentials(email, password)) {
-            System.out.println("Auth successful");
+            System.out.println("Auth for "+email+" successful");
             req.getSession().setAttribute("email", email);
             req.getServletContext().getRequestDispatcher("/authenticated.jsp").forward(req, resp);
         } else {
             System.out.println("Wrong or missing Credentials");
+            //TODO return to login page with wrong credentials notification
         }
     }
 }
