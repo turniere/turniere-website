098ea44b116dba2fe737f1d65d0375f6e1a9a8cf
Malaber <32635600+Malaber@users.noreply.github.com>
2018-06-24 18:00:14 +0200

    remove unused imports to protect the server from evil things that would happen otherwise

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,13 +1,9 @@
 package database;
 
-import database.models.User;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
-import java.sql.*;
 
 abstract class DatabaseManager {
     private static final SessionFactory ourSessionFactory;
