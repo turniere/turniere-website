2f2c96650978f3f0f678775d24492917673415ba
Thor77 <thor77@thor77.org>
2018-06-26 22:36:39 +0200

    Add Tournament model

---
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/models/Tournament.java
@@ -0,0 +1,39 @@
+package de.dhbw.karlsruhe.turniere.database.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tournaments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tournament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String code;
+
+    private String description;
+
+    private Boolean isPublic;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Team> teams;
+
+    public Tournament(String name, String code, String description, Boolean isPublic, List<Team> teams) {
+        this.name = name;
+        this.code = code;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.teams = teams;
+    }
+}
