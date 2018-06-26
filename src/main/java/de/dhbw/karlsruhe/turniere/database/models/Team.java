c8c38c065f96130dfa15363248baeaeba7c98b3a
Thor77 <thor77@thor77.org>
2018-06-26 21:16:52 +0200

    Add entity-annotation and id

--- src/main/java/de/dhbw/karlsruhe/turniere/database/models/Team.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/models/Team.java
@@ -3,10 +3,19 @@ package de.dhbw.karlsruhe.turniere.database.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "teams")
 @Getter
 @Setter
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
+
     private Integer points;
 
     public Team(String name) {
