9f9b5a353972d53d2b8c62c5783160d5150deb01
Thor77 <thor77@thor77.org>
2018-06-26 21:14:21 +0200

    Replace g/setter with lombok annotation

--- src/main/java/de/dhbw/karlsruhe/turniere/database/models/Team.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/models/Team.java
@@ -1,5 +1,10 @@
 package de.dhbw.karlsruhe.turniere.database.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Team {
     private String name;
     private Integer points;
@@ -9,27 +14,7 @@ public class Team {
     }
 
     public Team(String name, Integer points) {
-
         this.name = name;
         this.points = points;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public Team setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public Integer getPoints() {
-        return points;
-    }
-
-    public Team setPoints(Integer points) {
-        this.points = points;
-        return this;
     }
 }
