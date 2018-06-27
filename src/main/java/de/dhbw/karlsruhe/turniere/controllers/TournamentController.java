0c72258ee086a95af47e1a59cb3ec6ec06bb76f4
Thor77 <thor77@thor77.org>
2018-06-27 10:51:25 +0200

    Add private tournaments to /tournaments

--- src/main/java/de/dhbw/karlsruhe/turniere/controllers/TournamentController.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/controllers/TournamentController.java
@@ -1,16 +1,22 @@
 package de.dhbw.karlsruhe.turniere.controllers;
 
+import de.dhbw.karlsruhe.turniere.authentication.CustomUserDetails;
+import de.dhbw.karlsruhe.turniere.database.models.Tournament;
+import de.dhbw.karlsruhe.turniere.database.models.User;
 import de.dhbw.karlsruhe.turniere.database.repositories.TournamentRepository;
 import de.dhbw.karlsruhe.turniere.forms.TournamentForm;
 import de.dhbw.karlsruhe.turniere.services.TournamentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,8 +25,20 @@ public class TournamentController {
     private final TournamentService tournamentService;
 
     @GetMapping("/tournaments")
-    String tournamentList(Model model) {
+    String tournamentList(Model model, Authentication authentication) {
+        // add public tournaments to template
         model.addAttribute("publicTournaments", tournamentRepository.findByIsPublic(true));
+        // check if user is logged in
+        if (authentication != null) {
+            // fetch user tournaments
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            User user = customUserDetails.getUser();
+            List<Tournament> userTournaments = user.getTournaments();
+            // add to template if not empty
+            if (!userTournaments.isEmpty()) {
+                model.addAttribute("privateTournaments", userTournaments);
+            }
+        }
         return "tournaments";
     }
 
