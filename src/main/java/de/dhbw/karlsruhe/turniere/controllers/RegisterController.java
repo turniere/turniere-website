6688a8baed670bc70ecd5bfa3c252f6bbab80b6d
Thor77 <thor77@thor77.org>
2018-06-27 13:07:03 +0200

    Autologin after register and redirect to index

--- src/main/java/de/dhbw/karlsruhe/turniere/controllers/RegisterController.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/controllers/RegisterController.java
@@ -1,16 +1,17 @@
 package de.dhbw.karlsruhe.turniere.controllers;
 
-import de.dhbw.karlsruhe.turniere.forms.RegisterForm;
 import de.dhbw.karlsruhe.turniere.authentication.UserService;
 import de.dhbw.karlsruhe.turniere.database.models.User;
+import de.dhbw.karlsruhe.turniere.forms.RegisterForm;
 import de.dhbw.karlsruhe.turniere.forms.validators.RegisterFormValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -26,13 +27,23 @@ public class RegisterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    String postRegister(@Valid RegisterForm registerForm, BindingResult bindingResult, Model model) {
+    String postRegister(@Valid RegisterForm registerForm, BindingResult bindingResult, HttpServletRequest
+            httpServletRequest) {
+        // valid form
         registerFormValidator.validate(registerForm, bindingResult);
         if (bindingResult.hasErrors()) {
             return "login";
         }
-        User user = new User(registerForm.getUsername(), registerForm.getEmail(), registerForm.getPassword());
-        userService.register(user);
-        return "redirect:/login";
+        // create and register user object
+        userService.register(new User(registerForm.getUsername(), registerForm.getEmail(), registerForm
+                .getPassword()));
+        // login newly registered user
+        try {
+            httpServletRequest.login(registerForm.getEmail(), registerForm.getPassword());
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+        // redirect to index
+        return "redirect:/";
     }
 }
