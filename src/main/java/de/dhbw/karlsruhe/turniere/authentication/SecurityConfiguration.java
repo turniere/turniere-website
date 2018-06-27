dd2cbde0185134217bdc913216d73826ca9e76e6
Felix Hamme <fhamme@united-internet.de>
2018-06-27 10:59:19 +0200

    permit access to static content for unauthenticated users

--- src/main/java/de/dhbw/karlsruhe/turniere/authentication/SecurityConfiguration.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/authentication/SecurityConfiguration.java
@@ -22,7 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeRequests().antMatchers("/", "/register").permitAll()
+                .authorizeRequests().antMatchers("/", "/register", "/css/**", "/fonts/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
