851f798f7a5f1aee0c8ccc3f3a36e0c535f1a870
Malaber <32635600+Malaber@users.noreply.github.com>
2018-06-28 17:02:55 +0200

    Add TournamentService.generateMatches() and implement it in TournamentService.create()

--- src/main/java/de/dhbw/karlsruhe/turniere/services/TournamentService.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/services/TournamentService.java
@@ -10,7 +10,9 @@ import de.dhbw.karlsruhe.turniere.database.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -28,6 +30,37 @@ public class TournamentService {
         return new Match(teams.get(0), teams.get(1), null, null, Match.State.NOT_STARTED);
     }
 
+    private int nextPowerOf2(int number){
+        Integer nextPower = 0;
+        for (int i = 0; nextPower<number;i++){
+            nextPower = (int)Math.pow(2,i);
+        }
+        return nextPower;
+    }
+
+    private List<Match> generateMatches(Tournament tournament, boolean randomize) {
+        List<Team> teams = tournament.getTeams();
+        if (randomize){
+            Collections.shuffle(teams);
+        }
+        //needed Games --> how many teams need to be kicked out to get the number of teams to the next lower power of 2
+        int neededGames;
+        if (nextPowerOf2(teams.size()) == teams.size()) {
+            neededGames = teams.size();
+        }else{
+            neededGames = teams.size()-nextPowerOf2(teams.size())/2;
+        }
+        List<Match> matches = new ArrayList<>();
+        //generate neededGames number of matches and add them to the list matches
+        for (int i = 0; i<neededGames; i++) {
+            matches.add(new Match(teams.get(0), teams.get(1), null, null, Match.State.NOT_STARTED));
+            teams.remove(0);
+            teams.remove(1);
+        }
+        return matches;
+
+    }
+
     public Tournament create(String name, String description, Boolean isPublic, String[] teamNames, User owner) {
         // create team objects from names and save them into a list
         List<Team> teams = Arrays.stream(teamNames).map(teamName -> teamRepository.save(new Team(teamName))).collect
@@ -36,7 +69,11 @@ public class TournamentService {
         String code = UUID.randomUUID().toString();
         // create and save tournament object
         Tournament tournament = tournamentRepository.save(new Tournament(name, code, description, isPublic, teams));
-        tournament.getMatches().add(nextMatch(tournament));
+        //tournament.getMatches().add(nextMatch(tournament)); // << deprecated Jonas Code
+        List<Match> matches = generateMatches(tournament,true);
+        for (Match match:matches){
+            tournament.getMatches().add(match);
+        }
         tournamentRepository.save(tournament);
         // add saved tournament object to authenticated user (owner)
         owner.getTournaments().add(tournament);
