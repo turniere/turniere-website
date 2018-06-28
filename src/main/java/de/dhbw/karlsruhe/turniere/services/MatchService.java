f04d73c521bbec7d6876140d70083fafd081a71d
Malaber <32635600+Malaber@users.noreply.github.com>
2018-06-29 00:47:01 +0200

    Add MatchService

---
+++ src/main/java/de/dhbw/karlsruhe/turniere/services/MatchService.java
@@ -0,0 +1,41 @@
+package de.dhbw.karlsruhe.turniere.services;
+
+import de.dhbw.karlsruhe.turniere.database.models.Match;
+import de.dhbw.karlsruhe.turniere.database.repositories.MatchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MatchService {
+    private MatchRepository matchRepository;
+
+    public void setResults(Match match, int scoreTeam1, int scoreTeam2) {
+        match.setScoreTeam1(scoreTeam1);
+        match.setScoreTeam2(scoreTeam2);
+        match.setState(evaluateWinner(match.getScoreTeam1(), match.getScoreTeam2()));
+        matchRepository.save(match);
+    }
+
+    public void setLivescore(Match match, int scoreTeam1, int scoreTeam2) {
+        match.setScoreTeam1(scoreTeam1);
+        match.setScoreTeam2(scoreTeam2);
+        match.setState(Match.State.IN_PROGRESS);
+        matchRepository.save(match);
+    }
+
+    public void whoWon(Match match){
+        match.setState(evaluateWinner(match.getScoreTeam1(),match.getScoreTeam2()));
+        matchRepository.save(match);
+    }
+
+    private Match.State evaluateWinner(int scoreTeam1, int scoreTeam2) {
+        if (scoreTeam1 < scoreTeam2) {
+            return Match.State.TEAM2_WON;
+        } else if (scoreTeam2 < scoreTeam1) {
+            return Match.State.TEAM1_WON;
+        } else {
+            return Match.State.IN_PROGRESS;
+        }
+    }
+}
