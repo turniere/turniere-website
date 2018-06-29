69418b5f2798ed47f86627c8925bea47eb4f7aa0
Thor77 <thor77@thor77.org>
2018-06-29 17:35:06 +0200

    Add equals() and hashCode() to compare users
    required for ownership checking in MatchController

--- src/main/java/de/dhbw/karlsruhe/turniere/database/models/User.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/models/User.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -33,5 +34,18 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
