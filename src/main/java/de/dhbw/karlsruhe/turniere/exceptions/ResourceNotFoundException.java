a8de908fd84d31d41e7ffcfc63b9a14df41efef5
Thor77 <thor77@thor77.org>
2018-06-29 17:14:56 +0200

    Add ResourceNotFoundException (404)
    to easily generate a 404 error page with a meaningful message

---
+++ src/main/java/de/dhbw/karlsruhe/turniere/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package de.dhbw.karlsruhe.turniere.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
+}
