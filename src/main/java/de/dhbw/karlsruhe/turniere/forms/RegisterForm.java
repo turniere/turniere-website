fec5112db685d9f843899ac497f06dd5418276bd
Thor77 <thor77@thor77.org>
2018-07-04 14:26:09 +0200

    Fix Getter/Setter only removed from RegisterForm

--- src/main/java/de/dhbw/karlsruhe/turniere/forms/RegisterForm.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/forms/RegisterForm.java
@@ -1,9 +1,14 @@
 package de.dhbw.karlsruhe.turniere.forms;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Getter
+@Setter
 public class RegisterForm {
     @NotEmpty
     private String username;
