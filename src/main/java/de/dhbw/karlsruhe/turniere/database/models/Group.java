23f938dff7a77bdf33bd7992faa08df841812e04
Malaber <32635600+Malaber@users.noreply.github.com>
2018-07-05 09:34:42 +0200

    Adjust Lombock in Group Model

--- src/main/java/de/dhbw/karlsruhe/turniere/database/models/Group.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/models/Group.java
@@ -1,11 +1,22 @@
 package de.dhbw.karlsruhe.turniere.database.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
