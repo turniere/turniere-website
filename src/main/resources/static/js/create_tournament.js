87d1bb36758fd8a38de9b491cc067d6da610b1de
tf98 <tobiasfritz@outlook.de>
2018-07-11 14:31:37 +0200

    Prevent only Whitespaces as Teamname on create page

--- src/main/resources/static/js/create_tournament.js
+++ src/main/resources/static/js/create_tournament.js
@@ -10,61 +10,77 @@ var errorMessage = document.getElementById("f-teamname-error");
 
 function addTeam(inputString) {
     if (inputString !== "") {
-        if (teamAlreadyExistend()) {
-            showErrorMessage();
-        } else {
-            teamnames.push(inputString);
-            var div = document.createElement("div");
-            div.setAttribute("class", "h-auto alert alert-dismissible alert-success shadow-sm fade show");
-            div.setAttribute("style", "display: inline-block; margin-right: 5px");
-            var node = document.createTextNode(inputString);
-            div.appendChild(node);
+        if (!teamnameEmpty()) {
+            if (teamAlreadyExistend()) {
+                showErrorMessage("Jeder Teamname darf nur einmal vergeben werden!");
+            } else {
+                teamnames.push(inputString);
+                var div = document.createElement("div");
+                div.setAttribute("class", "h-auto alert alert-dismissible alert-success shadow-sm fade show");
+                div.setAttribute("style", "display: inline-block; margin-right: 5px");
+                var node = document.createTextNode(inputString);
+                div.appendChild(node);
 
-            var btn = document.createElement("button");
-            btn.setAttribute("type", "button");
-            btn.setAttribute("class", "close");
-            btn.setAttribute("data-dismiss", "alert");
-            btn.setAttribute("aria-label", "Close");
+                var btn = document.createElement("button");
+                btn.setAttribute("type", "button");
+                btn.setAttribute("class", "close");
+                btn.setAttribute("data-dismiss", "alert");
+                btn.setAttribute("aria-label", "Close");
 
-            var span = document.createElement("span");
-            span.setAttribute("aria-hidden", "true");
-            span.innerHTML = "&times;";
+                var span = document.createElement("span");
+                span.setAttribute("aria-hidden", "true");
+                span.innerHTML = "&times;";
 
-            btn.appendChild(span);
+                btn.appendChild(span);
 
-            btn.addEventListener("click", function () {
-                var firstText = "";
-                for (var i = 0; i < div.childNodes.length; i++) {
-                    var curNode = div.childNodes[i];
-                    if (curNode.nodeName === "#text") {
-                        firstText = curNode.nodeValue;
-                        break;
+                btn.addEventListener("click", function () {
+                    var firstText = "";
+                    for (var i = 0; i < div.childNodes.length; i++) {
+                        var curNode = div.childNodes[i];
+                        if (curNode.nodeName === "#text") {
+                            firstText = curNode.nodeValue;
+                            break;
+                        }
                     }
-                }
-                var index = teamnames.lastIndexOf(firstText);
-                teamnames.splice(index, 1);
-                element.removeChild(div);
-            });
+                    var index = teamnames.lastIndexOf(firstText);
+                    teamnames.splice(index, 1);
+                    element.removeChild(div);
+                });
 
-            div.appendChild(btn);
-            element.appendChild(div);
+                div.appendChild(btn);
+                element.appendChild(div);
 
-            input.value = '';
+                input.value = '';
+            }
+        } else {
+            showErrorMessage("Der Teamname darf nicht nur aus Leerzeichen bestehen!");
         }
+    } else {
+        showErrorMessage("Bitte geben Sie einen Teamnamen ein!");
     }
+
 }
 
 function addTeamList() {
-    if (input.value.includes(",")) {
-        var teamList = input.value.split(",");
-        for (var i = 0; i < teamList.length; i++) {
-            addTeam(teamList[i]);
+    if (!teamnameEmpty()) {
+        if (input.value.includes(",")) {
+            var teamList = input.value.split(",");
+            for (var i = 0; i < teamList.length; i++) {
+                addTeam(teamList[i]);
+            }
         }
+    } else {
+        showErrorMessage();
     }
 }
 
 function teamAlreadyExistend() {
     return teamnames.indexOf(input.value) !== -1;
+}
+
+function teamnameEmpty() {
+    var str = input.value.replace(/\s/g, "");
+    return !(str.length > 0);
 }
 
 addBtn.addEventListener("click", function (ev) {
@@ -73,7 +89,6 @@ addBtn.addEventListener("click", function (ev) {
 });
 
 input.addEventListener("keyup", function (ev) {
-
     if (ev.keyCode === 13) {
         addTeamList();
         addTeam(input.value);
@@ -86,19 +101,20 @@ submitBtn.addEventListener("click", function () {
     }
 });
 
+function showErrorMessage(message) {
+    errorMessage.innerText = message;
+    errorMessage.style.display = "block";
+    setTimeout(function () {
+        errorMessage.style.display = "none"
+    }, 3000);
+}
+
 function stopRKey(evt) {
     evt = (evt) ? evt : ((event) ? event : null);
     var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
     if ((evt.keyCode === 13) && (node.type === "text")) {
         return false;
     }
-}
-
-function showErrorMessage() {
-    errorMessage.style.display = "block";
-    setTimeout(function () {
-        errorMessage.style.display = "none"
-    }, 3000);
 }
 
 document.onkeypress = stopRKey;
