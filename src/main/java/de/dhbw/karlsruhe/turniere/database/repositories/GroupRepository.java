a3bb71602de04115d0b95501c30ae692339818c1
Malaber <32635600+Malaber@users.noreply.github.com>
2018-07-11 11:56:55 +0200

    Add findByTeamsContains Method

--- src/main/java/de/dhbw/karlsruhe/turniere/database/repositories/GroupRepository.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/database/repositories/GroupRepository.java
@@ -2,8 +2,11 @@ package de.dhbw.karlsruhe.turniere.database.repositories;
 
 import de.dhbw.karlsruhe.turniere.database.models.Group;
 import de.dhbw.karlsruhe.turniere.database.models.Match;
+import de.dhbw.karlsruhe.turniere.database.models.Team;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GroupRepository extends CrudRepository<Group, Long> {
     Group findByMatchesContains(Match match);
+    Group findByTeamsContains(Team team);
+
 }
