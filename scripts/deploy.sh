44ddc198d8a09eb82b55e6c35bad9d7818f3413c
Thor77 <thor77@thor77.org>
2018-10-18 14:55:32 +0200

    Build and push docker images via travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-mvn clean package
-echo "Moving package to remote host"
-rsync target/turniere-*.jar turniere@dev.turnie.re:turniere.jar
-echo "rsync exited with $?"
+mvn docker:build
+mvn docker:push
