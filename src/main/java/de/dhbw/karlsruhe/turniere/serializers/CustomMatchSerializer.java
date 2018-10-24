2741de9c527616d2489d2de7508bdd74acc5bc36
Malaber <32635600+Malaber@users.noreply.github.com>
2018-10-24 22:18:41 +0200

    CP Correct Nullpointer Exception in JSON Serializer

--- src/main/java/de/dhbw/karlsruhe/turniere/serializers/CustomMatchSerializer.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/serializers/CustomMatchSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import de.dhbw.karlsruhe.turniere.database.models.Match;
+import de.dhbw.karlsruhe.turniere.database.models.Team;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
@@ -13,9 +14,20 @@ import java.util.Optional;
 public class CustomMatchSerializer extends JsonSerializer<Match> {
     @Override
     public void serialize(Match match, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        Optional<Team> optionalTeam1 = Optional.ofNullable(match.getTeam1());
+        String name1 = null;
+        if (optionalTeam1.isPresent()){
+            name1 = optionalTeam1.get().getName();
+        }
+        Optional<Team> optionalTeam2 = Optional.ofNullable(match.getTeam2());
+        String name2 = null;
+        if (optionalTeam2.isPresent()){
+            name2 = optionalTeam2.get().getName();
+        }
+
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeStringField("name1", match.getTeam1().getName());
-        jsonGenerator.writeStringField("name2", match.getTeam2().getName());
+        jsonGenerator.writeStringField("name1", name1);
+        jsonGenerator.writeStringField("name2", name2);
         jsonGenerator.writeNumberField("score1", Optional.ofNullable(match.getScoreTeam1()).orElse(0));
         jsonGenerator.writeNumberField("score2", Optional.ofNullable(match.getScoreTeam2()).orElse(0));
         jsonGenerator.writeBooleanField("live", match.getState() == Match.State.IN_PROGRESS);
