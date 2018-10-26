8686ad33e60d41abffd4932ae01d94fce5527295
Malaber <32635600+Malaber@users.noreply.github.com>
2018-10-26 15:50:22 +0200

    Read mailtext from static file

--- src/main/java/de/dhbw/karlsruhe/turniere/services/MailService.java
+++ src/main/java/de/dhbw/karlsruhe/turniere/services/MailService.java
@@ -7,10 +7,17 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,20 +27,41 @@ public class MailService {
     private final JavaMailSender javaMailSender;
     private final VerificationTokenRepository verificationTokenRepository;
 
+    private String mailText;
+    private String mailFrom;
+    private String mailSubject;
+
+    @PostConstruct
+    private void init() {
+        mailText = readMailText(environment.getProperty("spring.mail.textPath", "mail.txt"));
+        mailFrom = Objects.requireNonNull(environment.getProperty("spring.mail.from"));
+        mailSubject = Objects.requireNonNull(environment.getProperty("spring.mail.subject"));
+    }
+
+    private String readMailText(String filepath) {
+        File file = new File(filepath);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String fileText = br.lines().collect(Collectors.joining("\n"));
+            return fileText;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void sendRegistrationMail(VerificationToken verificationToken) {
         User user = verificationToken.getUser();
         String recipient = user.getEmail();
         String token = verificationToken.getToken();
         String url = environment.getProperty("spring.root_url") + "/confirm_registration?token=" + token;
-        String debugMessage = String.format("%s (token: %s)", recipient, token);
-        logger.info("Sending activation mail to " + debugMessage);
+        String debugMessage = String.format("mail to %s (token: %s)", recipient, token);
+        logger.info("Sending activation " + debugMessage);
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipient);
-        email.setFrom(environment.getProperty("spring.mail.from"));
-        email.setSubject("Turnie.re registration");
-        email.setText(url);
+        email.setFrom(mailFrom);
+        email.setSubject(mailSubject);
+        email.setText(String.format(mailText, url));
         javaMailSender.send(email);
-        logger.info("Success sending " + debugMessage);
+        logger.info("Success fully sent " + debugMessage);
         verificationToken.setSent(true);
         verificationTokenRepository.save(verificationToken);
     }
